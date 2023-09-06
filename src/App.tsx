0467966a8266c08dc89eadef5c2b89dfedea93d2
KimYuKyung <kyg0954@naver.com>
2023-09-06 17:15:32 +0900

    feat: globalstyle 적용

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,17 @@
 import React from 'react';
 import './App.css';
+import GlobalStyle from '../src/styles/GlobalStyle';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <GlobalStyle />
+      <Router>
+        <Routes></Routes>
+      </Router>
+    </div>
+  );
 }
 
 export default App;
