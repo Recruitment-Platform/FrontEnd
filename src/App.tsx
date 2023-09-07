610065d605b6613bc54f8902551d568a3481f71a
KimYuKyung <kyg0954@naver.com>
2023-09-07 17:25:27 +0900

    test: 빌드 테스트를 위해 테스트 페이지 작성

--- src/App.tsx
+++ src/App.tsx
@@ -2,13 +2,16 @@ import React from 'react';
 import './App.css';
 import GlobalStyle from '../src/styles/GlobalStyle';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Test from '@pages/test';
 
 function App() {
   return (
     <div className="App">
       <GlobalStyle />
       <Router>
-        <Routes></Routes>
+        <Routes>
+          <Route path="/" element={<Test />}></Route>
+        </Routes>
       </Router>
     </div>
   );
---
+++ src/pages/test.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function Test() {
+  return (
+    <div>
+      <h1>테스트 페이지입니다.</h1>
+    </div>
+  );
+}
+
+export default Test;
