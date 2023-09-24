b7cae8acb2ad1035cf1750845551a8fad9a29ff3
송혜정 <songhj10207@naver.com>
2023-09-24 22:03:28 +0900

    layout/header: Tag 삭제로 main header 코드 수정

--- src/components/layout/MainHeader.tsx
+++ src/components/layout/MainHeader.tsx
@@ -2,26 +2,12 @@ import React from 'react';
 import styled from 'styled-components';
 import Nav from './Nav';
 import SearchBar from '@components/layout/SearchBar';
-import RecommendTag from './recommendTag';
-
 function MainHeader() {
-  const recommendtags = [
-    'JavaScript',
-    'HTML',
-    'CSS',
-    'Front-end',
-    'Adobe Photoshop',
-  ];
   return (
     <>
       <MainHeaderContainer>
         <Nav />
         <SearchBar />
-        <Tags>
-          {recommendtags.map((tag) => {
-            return <RecommendTag key={tag} name={tag} />;
-          })}
-        </Tags>
       </MainHeaderContainer>
     </>
   );
@@ -30,7 +16,7 @@ function MainHeader() {
 export default MainHeader;
 
 const MainHeaderContainer = styled.header`
-  background: rgba(41, 102, 255, 0.1);
+  background: rgba(242, 244, 255, 1);
   width: 100%;
   max-width: 1440px;
   height: 250px;
@@ -38,11 +24,4 @@ const MainHeaderContainer = styled.header`
   flex-direction: column;
   text-align: center;
   align-items: center;
-  justify-content: space-between;
-`;
-
-const Tags = styled.div`
-  display: flex;
-  gap: 10px;
-  margin-bottom: 20px;
 `;
