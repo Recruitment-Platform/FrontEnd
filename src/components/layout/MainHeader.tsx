cca9ca7535ff9081105768330493fede4c7fba18
KimYuKyung <kyg0954@naver.com>
2023-09-28 21:46:06 +0900

    feat: MainHeader 컴포넌트 디자인 수정

--- src/components/layout/MainHeader.tsx
+++ src/components/layout/MainHeader.tsx
@@ -2,26 +2,57 @@ import React from 'react';
 import styled from 'styled-components';
 import Nav from './Nav';
 import SearchBar from '@components/layout/SearchBar';
-function MainHeader() {
+
+interface MainHeaderProps {
+  backgroundColor?: string; // 헤더 배경화면 색
+  shadow?: boolean; // 헤더 그림자 여부
+}
+
+function MainHeader({
+  backgroundColor = '#ffffff',
+  shadow = true,
+}: MainHeaderProps) {
   return (
     <>
-      <MainHeaderContainer>
-        <Nav />
+      <MainHeaderContainer $backgroundColor={backgroundColor} $shadow={shadow}>
+        <HeaderBox>
+          <Nav />
+        </HeaderBox>
         <SearchBar />
       </MainHeaderContainer>
     </>
   );
 }
 
-export default MainHeader;
-
-const MainHeaderContainer = styled.header`
-  background: rgba(242, 244, 255, 1);
-  width: 100%;
-  max-width: 1440px;
-  height: 250px;
+const MainHeaderContainer = styled.header<{
+  $backgroundColor: string;
+  $shadow: boolean;
+}>`
   display: flex;
   flex-direction: column;
   text-align: center;
   align-items: center;
+  width: 100%;
+  height: auto;
+  /* background: rgba(242, 244, 255, 1); */
+  background: ${(props) => props.$backgroundColor};
+  margin: 0 auto;
+  box-shadow: ${(props) =>
+    props.$shadow ? '0px 0px 20px 0px rgba(0, 0, 0, 0.05)' : 'none'};
+
+  @media screen and (max-width: 768px) {
+    box-shadow: none;
+  }
 `;
+
+const HeaderBox = styled.div`
+  width: 100%;
+  height: 115px;
+  max-width: 1440px;
+
+  @media screen and (max-width: 768px) {
+    height: 137px;
+  }
+`;
+
+export default MainHeader;
