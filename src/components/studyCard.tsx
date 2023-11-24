82cf19e1c8bffd325b64e0981469b125e40af1af
KimYuKyung <kyg0954@naver.com>
2023-11-24 23:15:35 +0900

    fix: Home 페이지 반응형 수정, studyCard 컴포넌트 반응형 수정
    - grid 방식으로 변경
    - font-size 단위를 rem으로 변경

--- src/components/studyCard.tsx
+++ src/components/studyCard.tsx
@@ -13,19 +13,35 @@ interface StudyCardProps {
 function StudyCard({ date, mode, title, content, tags }: StudyCardProps) {
   return (
     <StudyCardLayout>
-      <DateAndMode>
-        <Date>{date}</Date>
-        <Mode>{mode}</Mode>
-      </DateAndMode>
-      <Contents>
-        <Title>{title}</Title>
-        <Content>{content}</Content>
-      </Contents>
-      <Tags>
-        {tags.map((tag) => {
-          return <TechTag key={tag} name={tag} />;
-        })}
-      </Tags>
+      <svg
+        width="100%"
+        height="100%"
+        viewBox="0 0 500 280"
+        preserveAspectRatio="xMinYMin meet"
+      >
+        <foreignObject
+          width="100%"
+          height="100%"
+          xmlns="http://www.w3.org/1999/xhtml"
+        >
+          <CardBox>
+            <DateAndMode>
+              <Date>{date}</Date>
+              <Mode>{mode}</Mode>
+            </DateAndMode>
+            <Contents>
+              <Title>{title}</Title>
+              <Content>{content}</Content>
+            </Contents>
+            <Tags $visible={Boolean(tags.length - 3)}>
+              {tags.map((tag) => {
+                return <TechTag key={tag} name={tag} />;
+              })}
+              <p>+{tags.length - 3}</p>
+            </Tags>
+          </CardBox>
+        </foreignObject>
+      </svg>
     </StudyCardLayout>
   );
 }
@@ -33,21 +49,21 @@ function StudyCard({ date, mode, title, content, tags }: StudyCardProps) {
 const StudyCardLayout = styled.div`
   display: flex;
   flex-direction: column;
-  margin-top: 10px; /*추가한 코드*/
   align-items: flex-start;
-  width: 500px;
-  /* height: 280px; */
   border-radius: 15px;
   border: 1px solid #e1e1e1;
   background: #ffffff;
-  box-shadow: 0px 0px 20px 0px rgba(0, 0, 0, 0.05);
-  padding: 35px;
-  @media screen and (max-width: 768px) {
-    width: 340px;
-  }
-  @media screen and (max-width: 390px) {
-    width: 250px;
-  }
+  width: 100%;
+  height: 100%;
+  overflow: hidden;
+  cursor: pointer;
+`;
+
+const CardBox = styled.div`
+  width: 100%;
+  height: 100%;
+  padding: 36px 35px;
+  position: relative;
 `;
 
 const DateAndMode = styled.div`
@@ -64,9 +80,6 @@ const Date = styled.span`
   font-style: normal;
   font-weight: 400;
   line-height: normal;
-  @media screen and (max-width: 768px) {
-    font-size: 13px;
-  }
 `;
 
 const Mode = styled.span`
@@ -79,6 +92,10 @@ const Mode = styled.span`
   font-weight: 700;
   line-height: normal;
   padding: 2px 5px;
+
+  @media screen and (max-width: 768px) {
+    padding: 2px 12px;
+  }
 `;
 
 const Contents = styled.div`
@@ -89,6 +106,10 @@ const Contents = styled.div`
   padding-top: 16px;
   padding-bottom: 23px;
   gap: 2px;
+
+  @media screen and (max-width: 768px) {
+    gap: 7px;
+  }
 `;
 
 const Title = styled.h3`
@@ -97,29 +118,61 @@ const Title = styled.h3`
   font-style: normal;
   font-weight: 700;
   line-height: normal;
-  @media screen and (max-width: 768px) {
-    font-size: 15px;
-  }
+  text-overflow: ellipsis;
+  overflow: hidden;
+  white-space: nowrap;
+  text-align: left;
+  width: 100%;
 `;
 
 const Content = styled.p`
   color: #8a8a8a;
   font-family: Noto Sans KR;
-  font-size: 1.6rem;
+  font-size: 1.6em;
   font-style: normal;
   font-weight: 400;
   line-height: normal;
   text-align: left;
+  width: 100%;
+  display: -webkit-box;
+  word-wrap: break-word;
+  -webkit-line-clamp: 4;
+  -webkit-box-orient: vertical;
+  text-overflow: ellipsis;
+  overflow: hidden;
+
   @media screen and (max-width: 768px) {
-    font-size: 13px;
+    -webkit-line-clamp: 2;
   }
 `;
 
-const Tags = styled.div`
+const Tags = styled.div<{ $visible: boolean }>`
   display: flex;
   flex-direction: row;
-  flex-wrap: wrap;
+  align-items: center;
   gap: 10px;
+  position: absolute;
+  bottom: 35px;
+
+  & > button:nth-child(n + 4) {
+    // 4번째부터 적용
+    display: none;
+  }
+
+  & > p {
+    display: ${(props) => (props.$visible ? 'block' : 'none')};
+    color: #8a8a8a;
+    font-size: 1.5rem;
+    font-style: normal;
+    font-weight: 400;
+    line-height: normal;
+    text-align: center;
+  }
+
+  @media screen and (max-width: 768px) {
+    width: 70%;
+    flex-wrap: wrap;
+  }
 `;
 
 export default StudyCard;
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -9,16 +9,27 @@ import { ReactComponent as UnreadCountIcon } from '@assets/icons/chat-new-icon.s
 import MainHeader from '@components/layout/MainHeader';
 import SearchBar from '@components/layout/SearchBar';
 import ChatList from '@components/layout/ChatList';
+
 /*임시로 설정한 데이터 */
 const DUMMYDATA = [
   {
     id: '1',
     date: '2023.09.27',
     mode: '온라인',
-    title: '함께하는 프로그래밍 스터디 그룹을 만듭니다!',
+    title:
+      '함께하는 프로그래밍 스터디 그룹을 만듭니다!함께하는 프로그래밍 스터디 그룹을 만듭니다!',
     content:
-      '저희는 함께 공부하고 서로 발전하는 프로그래밍 스터디 그룹을 찾고있습니다. 함께 공부하며 지식을 공유하고, 프로젝트를 개발하며 실력을 키우고자 합니다. 같이 열심히 노력하고 성장하고 싶으신 분들을 모집합니다!',
-    tags: ['JavaScript', 'HTML', 'CSS', 'Front-end'],
+      '저희는 함께 공부하고 서로 발전하는 프로그래밍 스터디 그룹을 찾고있습니다. 함께 공부하며 지식을 공유하고, 프로젝트를 개발하며 실력을 키우고자 합니다. 같이 열심히 노력하고 성장하고 싶으신 분들을 모집합니다!저희는 함께 공부하고 서로 발전하는 프로그래밍 스터디 그룹을 찾고있습니다. 함께 공부하며 지식을 공유하고, 프로젝트를 개발하며 실력을 키우고자 합니다. 같이 열심히 노력하고 성장하고 싶으신 분들을 모집합니다!',
+    tags: [
+      'JavaScript',
+      'HTML',
+      'CSS',
+      'Front-end',
+      'JavaScript',
+      'HTML',
+      'CSS',
+      'Front-end',
+    ],
   },
   {
     id: '2',
@@ -100,21 +111,103 @@ export default Home;
 
 const MainPageLayout = styled.div``;
 
-const UnreadCount = styled.div`
-  right: 5px;
-  position: absolute;
-  top: -5px;
-`;
-
-const ChatBox = styled.div`
-  top: 70px;
-  position: absolute;
-`;
-
 const MainHeaderCol = styled.div`
   width: 100%;
   height: 250px;
   background-color: rgba(242, 244, 255, 1);
+`;
+
+const MainContent = styled.div`
+  display: flex;
+  flex-direction: column;
+  flex-wrap: wrap;
+  align-items: center;
+  max-width: 1440px;
+  width: 100%;
+  height: auto;
+  min-height: calc(100vh - 425px);
+  padding: 0 20px;
+  margin: 0 auto;
+`;
+
+const MainContentNav = styled.div`
+  display: flex;
+  justify-content: flex-start;
+  width: 100%;
+  margin: 40px 0 25px 0;
+  max-width: 1020px;
+
+  & > li {
+    display: none;
+  }
+  & > p {
+    font-family: Noto Sans KR;
+    font-size: 2rem;
+    font-weight: 700;
+    line-height: 29px;
+    text-align: left;
+  }
+
+  @media screen and (max-width: 768px) {
+    justify-content: flex-end;
+    margin: 19px 0 25px 0;
+
+    & > p {
+      display: none;
+    }
+    & > li {
+      display: flex;
+      gap: 10px;
+      margin-left: 10px;
+
+      & > svg {
+        margin-top: 5px;
+      }
+      & > a {
+        font-family: Noto Sans KR;
+        color: rgba(89, 87, 87, 1);
+        font-size: 1.3rem;
+        font-weight: 400;
+        line-height: 19px;
+        letter-spacing: 0em;
+        text-align: center;
+        cursor: pointer;
+        &:hover {
+          font-weight: 700;
+          color: rgba(41, 102, 255, 1);
+        }
+      }
+    }
+  }
+`;
+
+const PostList = styled.div`
+  display: grid;
+  justify-content: center;
+  width: 100%;
+  grid-template-columns: repeat(2, minmax(auto, 500px)); // pc 화면일 땐 3줄
+  gap: 24px 19px;
+
+  @media screen and (max-width: 768px) {
+    grid-template-columns: repeat(1, minmax(auto, 100%)); // pc 화면일 땐 3줄
+  }
+`;
+
+const MoreData = styled.button`
+  margin: 40px;
+  background-color: transparent;
+  color: rgba(138, 138, 138, 1);
+  font-family: Noto Sans KR;
+  font-size: 1.5rem;
+  font-weight: 400;
+  line-height: 22px;
+  letter-spacing: 0em;
+  text-align: center;
+  cursor: pointer;
+
+  &:hover {
+    font-size: 1.6rem;
+  }
 `;
 
 const Icon = styled.li`
@@ -126,96 +219,15 @@ const Icon = styled.li`
   right: 5%;
 `;
 
-const PostList = styled.div`
-  gap: 40px;
-  display: flex;
-  flex-wrap: wrap;
-  justify-content: center;
+const ChatBox = styled.div`
+  top: 70px;
+  position: absolute;
 `;
 
-const MainContent = styled.div`
-  display: flex;
-  flex-direction: column;
-  flex-wrap: wrap;
-  justify-content: center;
-  align-items: center;
-  max-width: 1440px;
-  width: 100%;
-  height: auto;
-  padding: 0 20px;
-  margin: 0 auto;
-  @media screen and (max-width: 768px) {
-    padding-top: 20px;
-    max-width: 700px;
-  }
-`;
-
-const MainContentNav = styled.div`
-  display: flex;
-  width: 100%;
-  margin-top: 40px;
-  margin-bottom: 10px;
-  max-width: 1040px;
-  justify-content: flex-start;
-  & > li {
-    display: none;
-  }
-  & > p {
-    padding-left: 10px;
-    font-family: Noto Sans KR;
-    font-size: 20px;
-    font-weight: 700;
-    line-height: 29px;
-    text-align: left;
-  }
-  @media screen and (max-width: 1080px) {
-    max-width: 500px;
-  }
-  @media screen and (max-width: 768px) {
-    justify-content: flex-end;
-    margin-top: 0px;
-    max-width: 340px;
-    & > p {
-      display: none;
-    }
-    & > li {
-      padding-right:30px;
-      display: flex;
-      gap: 10px;
-      margin-left:10px;
-        & > svg {
-          margin-top: 5px;
-        }
-        & > a {
-          font-family: Noto Sans KR;
-          color: rgba(89, 87, 87, 1);
-          font-size: 13px;
-          font-weight: 400;
-          line-height: 19px;
-          letter-spacing: 0em;
-          text-align: center;         
-          & : hover {
-            font-weight: 700;
-            color: rgba(41, 102, 255, 1);
-            ck;   
-      }
-  }
-`;
-
-const MoreData = styled.button`
-  margin: 40px;
-  background-color: transparent;
-  color: rgba(138, 138, 138, 1);
-  font-family: Noto Sans KR;
-  font-size: 15px;
-  font-weight: 400;
-  line-height: 22px;
-  letter-spacing: 0em;
-  text-align: center;
-
-  &:hover {
-    font-size: 16px;
-  }
+const UnreadCount = styled.div`
+  right: 5px;
+  position: absolute;
+  top: -5px;
 `;
 
 const MainFooter = styled.div`
@@ -226,21 +238,39 @@ const MainFooter = styled.div`
   height: 175px;
   width: 100%;
   background: rgba(249, 249, 249, 1);
+
   & > p {
     font-family: Noto Sans KR;
-    font-size: 15px;
+    font-size: 1.5rem;
     line-height: 22px;
     letter-spacing: 0em;
     text-align: center;
     color: rgba(119, 119, 119, 1);
-  }
-  & : first-child {
-    font-weight: 700;
-    padding-bottom: 20px;
-  }
-  &: ntn-child(n+2):ntn-child(-n + 3) {
-    & > span {
+
+    &:first-child {
       font-weight: 700;
+      padding-bottom: 20px;
+    }
+    &:nth-child(n + 2):nth-child(-n + 3) {
+      & > span {
+        font-weight: 700;
+      }
+    }
+  }
+
+  @media screen and (max-width: 768px) {
+    & > p {
+      font-size: 1.2rem;
+      line-height: 20px;
+
+      &:first-child {
+        padding-bottom: 10px;
+      }
+      &:nth-child(n + 2):nth-child(-n + 3) {
+        & > span {
+          font-weight: 700;
+        }
+      }
     }
   }
 `;
